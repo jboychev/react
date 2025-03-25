b77f21a8797d9805afa9dceb39f922544281fbdd
jboychev <jboychev@yahoo.com>
2025-03-25 21:51:16 +0200

    Counter

--- src/App.js
+++ src/App.js
@@ -2,13 +2,18 @@ import logo from "./logo.svg";
 import "./App.css";
 import Counter from "./Components/counter";
 
+function Header() {
+  return <h1>Hello React</h1>;
+}
+
 function App() {
-  return (
-    <div className="appWrapper">
-      <h1 className="reactHeadline">Hello React</h1>
-      <Counter />
-    </div>
-  );
+  // return (
+  //   <div className="appWrapper">
+  //     <h1 className="reactHeadline">Hello React</h1>
+  //     <Counter />
+  //   </div>
+  // );
+  return <Header />;
 }
 
 export default App;
